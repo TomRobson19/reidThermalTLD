9b20b585f20910991be8824520e2454c78832349
Tom Robson <tomrobson19@gmail.com>
2017-10-30 13:59:10 +0000

    Tracker written (untested)

--- src/tracker.cpp
+++ src/tracker.cpp
@@ -6,21 +6,57 @@
 #include <string>
 #include <algorithm>
 
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "cv.h"
+#include "highgui.h"
+
+#include "motld/MultiObjectTLD.h"
+
 using namespace cv;
 using namespace std;
-
-//functions required: addTarget, deleteTarget, updateAllTargets, getFrame
 
 class GenericTracker
 {
 public:
-    GenericTracker(VideoCapture capture)
-    {}
+    virtual void initialise();
+    virtual void addTarget();
+    virtual void deleteTarget();
+    virtual void update();
 };
 
 class MultiObjectTLDTracker : public GenericTracker
 {
 public:
-    MultiObjectTLDTracker(VideoCapture capture) : GenericTracker(capture)
-    {}
+    void initialise(int width, int height)
+    {
+        tracker(width, height, settings);
+    }
+    void addTarget(Rectangle boundingBox)
+    {
+    	//convert rectangle to ObjectBox
+        tracker.addObject(boundingBox);
+    }
+    void deleteTarget()
+    {
+    	//use the getObjectBoxes function
+    }
+    void update(Mat image)
+    {
+        IplImage *frame = &(IplImage(image));
+
+        unsigned char img[size * 3];
+
+        for(int j = 0; j < size; j++)
+        {
+            img[j] = frame->imageData[j * 3 + 2];
+            img[j + size] = frame->imageData[j * 3 + 1];
+            img[j + 2 * size] = frame->imageData[j * 3];
+        }
+        tracker.processFrame();
+    }
+private:
+    MultiObjectTLD tracker;
+    MOTLDSettings settings();
 };
