dc7b167c7f4b8b62874f1eea7e679e6ffe7184a8
Tom Robson <tomrobson19@gmail.com>
2017-12-29 16:14:52 +0000

    More network experimentation and new files

--- src/networkExperimentation.py
+++ src/networkExperimentation.py
@@ -15,7 +15,7 @@ np.random.seed(1337)  # for reproducibility
 import random
 from keras.datasets import mnist
 from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Input, Lambda
+from keras.layers import Dense, Dropout, Input, Lambda, Conv2D, MaxPooling2D, Activation, Flatten
 from keras.optimizers import RMSprop
 from keras import backend as K
 
@@ -57,16 +57,37 @@ def create_pairs(x, digit_indices):
     return np.array(pairs), np.array(labels)
 
 
-def create_base_network(input_dim):
-    '''Base network to be shared (eq. to feature extraction).
-    '''
-    seq = Sequential()
-    seq.add(Dense(128, input_shape=(input_dim,), activation='relu'))
-    seq.add(Dropout(0.1))
-    seq.add(Dense(128, activation='relu'))
-    seq.add(Dropout(0.1))
-    seq.add(Dense(128, activation='relu'))
-    return seq
+def create_base_network(input_shape, X_train):
+    #Base network to be shared (eq. to feature extraction).
+    
+    #input image dimensions
+    img_rows, img_cols = 28, 28
+    # number of convolutional filters to use
+    nb_filters = 16
+    # size of pooling area for max pooling
+    pool_size = (2, 2)
+    # convolution kernel size
+    kernel_size = (3, 3)
+
+    model = Sequential()
+    model.add(Conv2D(nb_filters, kernel_size, padding='valid', input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(Conv2D(nb_filters, kernel_size))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=pool_size))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    # model.add(Dropout(0.5))
+    # seq = Sequential()
+    # seq.add(Dense(128, input_shape=(input_dim,), activation='relu'))
+    # seq.add(Dropout(0.1))
+    # seq.add(Dense(128, activation='relu'))
+    # seq.add(Dropout(0.1))
+    # seq.add(Dense(128, activation='relu'))
+    return model
 
 
 def compute_accuracy(predictions, labels):
@@ -77,14 +98,18 @@ def compute_accuracy(predictions, labels):
 
 # the data, shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-X_train = X_train.reshape(60000, 784)
-X_test = X_test.reshape(10000, 784)
+# X_train = X_train.reshape(60000, 784)
+# X_test = X_test.reshape(10000, 784)
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+input_shape = (28, 28, 1)
+
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 X_train /= 255
 X_test /= 255
 input_dim = 784
-nb_epoch = 20
+num_epochs = 20
 
 # create training+test positive and negative pairs
 digit_indices = [np.where(y_train == i)[0] for i in range(10)]
@@ -94,10 +119,14 @@ digit_indices = [np.where(y_test == i)[0] for i in range(10)]
 te_pairs, te_y = create_pairs(X_test, digit_indices)
 
 # network definition
-base_network = create_base_network(input_dim)
+# base_network = create_base_network(input_dim, X_train)
+base_network = create_base_network(input_shape, X_train)
 
-input_a = Input(shape=(input_dim,))
-input_b = Input(shape=(input_dim,))
+# input_a = Input(shape=(input_dim,))
+# input_b = Input(shape=(input_dim,))
+input_a = Input(shape=input_shape)
+input_b = Input(shape=input_shape)
+
 
 # because we re-use the same instance `base_network`,
 # the weights of the network
@@ -107,15 +136,16 @@ processed_b = base_network(input_b)
 
 distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([processed_a, processed_b])
 
-model = Model(input=[input_a, input_b], output=distance)
+model = Model(inputs=[input_a, input_b], outputs=distance)
 
 # train
 rms = RMSprop()
-model.compile(loss=contrastive_loss, optimizer=rms)
+#model.compile(loss=contrastive_loss, optimizer=rms)
+model.compile(loss=contrastive_loss, optimizer='adadelta')
 model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
           validation_data=([te_pairs[:, 0], te_pairs[:, 1]], te_y),
           batch_size=128,
-          nb_epoch=nb_epoch)
+          epochs=num_epochs)
 
 # compute final accuracy on training and test sets
 pred = model.predict([tr_pairs[:, 0], tr_pairs[:, 1]])
