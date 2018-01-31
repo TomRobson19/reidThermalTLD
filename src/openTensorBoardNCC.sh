cdcc911c51a5fe7ba1efd3c74cc4891ec0a931c1
Tom Robson <tomrobson19@gmail.com>
2018-01-31 16:01:54 +0000

    new scripts

--- src/openTensorBoardNCC.sh
+++ src/openTensorBoardNCC.sh
@@ -2,5 +2,7 @@
 #
 # Open Tensorboard in output directory
 #
+module load cuda/8.0-cudnn6
+
 xdg-open http://127.0.0.1:6006
 python3 /home/hzwr87/.local/lib/python3.5/site-packages/tensorboard/main.py --logdir=output/
