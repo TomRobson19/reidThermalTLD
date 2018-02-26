6f8e2f2d9f2d514c676e472acd152e7864a24fdf
Tom Robson <tomrobson19@gmail.com>
2018-02-26 10:46:17 +0000

    COnverting h5 to pb for use in dnn

---
+++ src/saved_models/conversionJobscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --gres=gpu:pascal:1
+#SBATCH -p debug
+#SBATCH --job-name=thermalReID
+
+#SBATCH -e stderr-filename
+#SBATCH -o stdout-filename
+
+#SBATCH --mem=8g
+##SBATCH -t 24:00:00
+
+module load cuda/8.0-cudnn6
+module load opencv/3.4-py3
+
+python3 keras_to_tensorflow.py -input_model_file openWorld.h5
