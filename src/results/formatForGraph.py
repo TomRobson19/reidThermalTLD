82286246217b7d86be4c1b0e4ea79d7aaea9fc19
Tom Robson <tomrobson19@gmail.com>
2018-04-01 11:43:16 +0100

    Data reformatting

---
+++ src/results/formatForGraph.py
@@ -0,0 +1,30 @@
+import csv
+import numpy as np
+positiveArray = []
+negativeArray = []
+
+with open('te_predictions_new.csv', 'r') as csvfile:
+     reader = csv.reader(csvfile, delimiter=',')
+     counter = 0
+     for row in reader:
+     	row = float(row[0])
+     	if counter % 2 == 0:
+     		positiveArray.append(row)
+     	else:
+     		negativeArray.append(row)
+     	counter += 1
+
+with open('tr_predictions_new.csv', 'r') as csvfile:
+     reader = csv.reader(csvfile, delimiter=',')
+     counter = 0
+     for row in reader:
+     	row = float(row[0])
+     	if counter % 2 == 0:
+     		positiveArray.append(row)
+     	else:
+     		negativeArray.append(row)
+     	counter += 1
+         
+
+np.savetxt("positive.csv",positiveArray, delimiter=",")
+np.savetxt("negative.csv",negativeArray, delimiter=",")
